4db5eb63b6f6628a4f2d8beb45eec9e1ad31f4b1
Wanley Rooby Alexis <alexroob96@gmail.com>
2025-01-21 12:50:38 -0300

    Projeto criado

---
+++ src/main/java/com/alexisfamily/aflist/AflistApplication.java
@@ -0,0 +1,13 @@
+package com.alexisfamily.aflist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AflistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AflistApplication.class, args);
+	}
+
+}
