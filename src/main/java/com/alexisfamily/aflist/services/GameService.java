0c1561703118b9d6ea547f2e328c722ddcd1793d
Wanley Rooby Alexis <alexroob96@gmail.com>
2025-01-22 12:29:12 -0300

    Get Game By Id

--- src/main/java/com/alexisfamily/aflist/services/GameService.java
+++ src/main/java/com/alexisfamily/aflist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.alexisfamily.aflist.dto.GameDTO;
 import com.alexisfamily.aflist.dto.GameMinDTO;
 import com.alexisfamily.aflist.entities.Game;
 import com.alexisfamily.aflist.repositories.GameRepository;
@@ -14,10 +16,19 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		// fazer um tratamento de exceção para tratar de um id que não existe
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
 
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
+
 }
