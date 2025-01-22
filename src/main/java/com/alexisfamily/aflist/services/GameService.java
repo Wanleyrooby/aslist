e68a62028d749c9453b3c53bc5598e943bb62f5c
Wanley Rooby Alexis <alexroob96@gmail.com>
2025-01-22 13:32:05 -0300

    SQL consult, projection and Get Games By List

--- src/main/java/com/alexisfamily/aflist/services/GameService.java
+++ src/main/java/com/alexisfamily/aflist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.alexisfamily.aflist.dto.GameDTO;
 import com.alexisfamily.aflist.dto.GameMinDTO;
 import com.alexisfamily.aflist.entities.Game;
+import com.alexisfamily.aflist.projections.GameMinProjection;
 import com.alexisfamily.aflist.repositories.GameRepository;
 
 @Service
@@ -28,6 +29,13 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
